016cc31a5591aef0dc7c249f96f01bfb90ad6851
Vikas Kundarapu <vikas.kundarapu@gmail.com>
2020-03-18 23:21:09 +0530

    Spring cloud config server that dynamically loads the configurations

---
+++ my-spring-cloud-config-server-dynamic-configuration/myspring-cloud-dynamic-configuration/src/test/java/com/vikas/springboot/dynamicconfig/MyspringCloudDynamicConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.springboot.dynamicconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringCloudDynamicConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
